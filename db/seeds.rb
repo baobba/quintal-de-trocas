df417abfbd15f4f5125eb8f06e5c530aea776e83
Osny Netto <netto16@gmail.com>
2016-07-11 20:17:21 -0300

    Add articles creation to seeds
    * Reference users randomly to toys

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.destroy_all
+AdminUser.create!(email: 'admin@example.com', password: 'admin123', password_confirmation: 'admin123')
 
 
 # create toy categories
@@ -29,7 +30,7 @@ end
 
 Toy.destroy_all
 (1..50).each_with_index do |a, index|
-  a=Toy.create title: "Brinquedo #{index}", description: "...", toy_category_id: ToyCategory.all.map(&:id).sample, toy_age_id: ToyAge.all.map(&:id).sample || 0, user: User.first
+  a=Toy.create title: "Brinquedo #{index}", description: "...", toy_category_id: ToyCategory.all.map(&:id).sample, toy_age_id: ToyAge.all.map(&:id).sample || 0, user_id: User.all.map(&:id).sample
   a.toy_images.create image: File.open(Rails.root.join('public', "toy_images/image0#{[*1..6].sample}.jpg"))
 end
 
@@ -45,4 +46,11 @@ places_names = ["Casa do Brincar", "Areté", "Playtoy", "Casa das Ideias"]
 ceps = ["07243-080", "93145-174", "13025-070", "02178-010", "07195-120", "37400-000", "07790-190", "90220-040", "18044-645", "13347-180", "11680-000", "13560-460", "93330-100", "85568-000", "05112-010", "12246-001", "95280-000", "09271-510", "54310-420", "03211-000", "01034-010", "42807-180", "04321-002", "03042-001", "12414-270", "18087-157", "04557-000", "09090-710", "06765-130", "13481-149"]
 (1..20).each_with_index do |index|
   Place.create title: places_names.sample, office_hours: "Seg. a Sex. das 9h as 18h", phone: "11-3032-2323", street: "Rua Ferreira de Araújo, 388", state: "SP", user: User.last, zipcode: ceps.sample
+end
+
+
+Article.destroy_all
+article_titles = ["A experiência da troca", "A troca na aldeia do Kamayuras", "Pedalando no Quintal, mas sem pedal ?"]
+(1..10).each_with_index do |index|
+  Article.create title: article_titles.sample, category: "Geral", user_id: User.all.map(&:id).sample, body: "Um dia do mês de fevereiro de 2014, estava eu navegando pela rede social Facebook, quando surgiu em meu feed de notícias um post chamado Quintal de Trocas, tratava-se de um vídeo muito interessante. Depois que o assisti fui pesquisar o assunto no site. E simplesmente fiquei encantada com o que descobri. Que ideia genial!!! - pensei. Expliquei para minha filha sobre a possibilidade de trocar seus brinquedos usados por outros e ela ficou bastante empolgada. Então selecionamos os brinquedos e em menos de 1 mês fizemos duas trocas. A primeira pelo correio com uma criança de Brasília, e a segunda pessoalmente, no Extra Anchieta/SP; experiências muito divertidas e também angustiosas, pois como é difícil para uma criança entre 0 a 10 anos aprender a esperar, aprender que nem tudo se resolve na hora que se quer, e nem do jeito que se deseja. (Foi um bom momento para ensinar o que significa planejar nossas ações)"
 end
