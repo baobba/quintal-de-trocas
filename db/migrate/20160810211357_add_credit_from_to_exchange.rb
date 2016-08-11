f40a77c1e242297436aa6bdbbe71a124b1c0e0dc
Osny Netto <netto@osny.me>
2016-08-11 17:13:38 -0300

    Update about us page with new text and a description about each collaborator

--- db/migrate/20160810211357_add_credit_from_to_exchange.rb
+++ db/migrate/20160810211357_add_credit_from_to_exchange.rb
@@ -2,13 +2,13 @@ class AddCreditFromToExchange < ActiveRecord::Migration
 
   def up
     add_column :exchanges, :credit_offer, :boolean
-    rename_column :credits, :is_available, :expired_at
-    change_column :credits, :expired_at, :datetime, :default => nil
+    add_column :credits, :expired_at, :datetime, :default => nil
+    # remove_column :exchanges, :is_available
   end
 
   def down
     remove_column :exchanges, :credit_offer
-    rename_column :credits, :expired_at, :is_available
-    change_column :credits, :is_available, :boolean
+    rename_column :credits, :expired_at
+    # add_column :exchanges, :is_available, :boolean
   end
 end
