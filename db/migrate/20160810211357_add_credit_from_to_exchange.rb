cdf175f99b20934a40327dd0f1f9dc22e9ced59a
Osny Netto <netto@osny.me>
2016-08-11 17:38:53 -0300

    Fix bugs on map

--- db/migrate/20160810211357_add_credit_from_to_exchange.rb
+++ db/migrate/20160810211357_add_credit_from_to_exchange.rb
@@ -3,12 +3,12 @@ class AddCreditFromToExchange < ActiveRecord::Migration
   def up
     add_column :exchanges, :credit_offer, :boolean
     add_column :credits, :expired_at, :datetime, :default => nil
-    # remove_column :exchanges, :is_available
+    remove_column :exchanges, :is_available
   end
 
   def down
     remove_column :exchanges, :credit_offer
     rename_column :credits, :expired_at
-    # add_column :exchanges, :is_available, :boolean
+    add_column :exchanges, :is_available, :boolean
   end
 end
