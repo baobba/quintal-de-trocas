791a8cecff69ac68cadb844568fa2e6a0a329717
Osny Netto <netto@osny.me>
2016-08-15 11:50:23 -0300

    Change birthday format on user form [2]

--- db/migrate/20160814214941_change_integer_format_in_users.rb
+++ db/migrate/20160814214941_change_integer_format_in_users.rb
@@ -1,9 +1,11 @@
 class ChangeIntegerFormatInUsers < ActiveRecord::Migration
-  def up
-    change_column :users, :birthday, :string
-  end
+  # def up
+  #   change_column :users, :birthday, :date, "USING birthday::date"
+  #   change_column :user_children, :birthday, :date, "USING birthday::date"
+  # end
 
-  def down
-    change_column :users, :birthday, :integer
-  end
+  # def down
+  #   change_column :users, :birthday, :integer
+  #   change_column :user_children, :birthday, :string
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810211357) do
+ActiveRecord::Schema.define(version: 20160814214941) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -244,7 +244,7 @@ ActiveRecord::Schema.define(version: 20160810211357) do
 
   create_table "user_children", force: :cascade do |t|
     t.string  "name"
-    t.string  "birthday"
+    t.date    "birthday"
     t.string  "gender"
     t.integer "user_id"
   end
@@ -266,7 +266,7 @@ ActiveRecord::Schema.define(version: 20160810211357) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "name"
-    t.integer  "birthday"
+    t.date     "birthday"
     t.string   "phone"
     t.string   "gender"
     t.string   "street"
