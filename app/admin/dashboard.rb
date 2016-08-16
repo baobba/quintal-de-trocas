14e43351244651fc3f25777c45a5fc6ee1d45397
Osny Netto <netto@osny.me>
2016-08-16 13:31:57 -0300

    Add some relevant statistic numbers on dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,45 +10,61 @@ ActiveAdmin.register_page "Dashboard" do
     #   end
     # end
 
-    "Hey"
-
     columns do
       column do
+        panel "Qtdade usuários" do
+          User.count
+        end
+      end
+      column do
+        panel "Qtdade brinquedos" do
+          Toy.count
+        end
+      end
+      column do
+        panel "Qtdade usuários com brinquedos cadastrados" do
+          User.joins(:toys).count
+        end
+      end
+      column do
+        panel "Qtdade Trocas" do
+          Exchange.count
+        end
+      end
+      column do
+        panel "Qtdade Trocas finalizadas" do
+          Exchange.where(finalized: true).count
+        end
+      end
+      column do
+        panel "Qtdade mensagens" do
+          ExchangeMessage.count
+        end
+      end
+      
+    end
+    columns do
+
+      column do
         panel "Últimos briquedos adicionados" do
-          Toy.order("created_at desc").limit(20).map do |toy|
-            li link_to(toy.title, toy_path(toy))
-            li "<strong>Faixa etária:</strong> #{toy.toy_age.title}&nbsp;&nbsp;".html_safe
-            li "<strong>Categoria:</strong> #{toy.toy_category.title}&nbsp;&nbsp;".html_safe
+          table_for Toy.order("created_at desc").limit(20) do
+            column "Title",        :title
+            column "Faixa etária", :toy_age_id
+            column "Categoria",    :toy_category_id
           end
         end
       end
       column do
         panel "Últimos usuários adicionados" do
-          User.order("created_at desc").limit(20).map do |user|
-            li user.email
+          table_for User.order("created_at desc").limit(20) do
+            column "Nome",   :name
+            column "Email",  :email
+            column "Cidade", :city
+            column "Estado", :state
           end
         end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
