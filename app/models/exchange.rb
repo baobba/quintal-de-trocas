00f29705ea6a82349e57f2195e4ecd6bf31dc5e8
Osny Netto <netto@osny.me>
2016-08-16 21:41:51 -0300

    Check if toy exists before showing them

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,8 +2,8 @@ class Exchange < ActiveRecord::Base
 
   acts_as_taggable
 
-  belongs_to :toy, :foreign_key => "toy_to"
-  belongs_to :user
+  belongs_to :toy, -> { with_deleted }, :foreign_key => "toy_to"
+  belongs_to :user, -> { with_deleted }
   has_many :exchange_messages
   has_one :credit
   accepts_nested_attributes_for :exchange_messages, 
@@ -15,7 +15,7 @@ class Exchange < ActiveRecord::Base
   acts_as_messageable
 
   def from_toy
-    Toy.find_by_id(toy_from)
+    Toy.with_deleted.find_by_id(toy_from)
   end
 
   def to_toy
@@ -27,7 +27,7 @@ class Exchange < ActiveRecord::Base
   end
 
   def to_user
-    Toy.find_by_id(toy_to).user
+    Toy.with_deleted.find_by_id(toy_to).user
   end
 
 end
