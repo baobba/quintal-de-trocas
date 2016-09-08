f5083450f3419842817e33a9f09f67a2f1a82f4a
Osny Netto <netto@osny.me>
2016-09-08 16:58:43 -0300

    Add new upload for toys with zoom and rotate options

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,11 +3,12 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"]        || '',
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]       || '',
-    # :region                 => ENV['AWS_REGION']            || '',
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]        || '',
+    # :region                 => ENV['AWS_REGION']          || '',
     # :path_style             => true
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                 || ''      # required
-  # config.fog_public     = false                                        # optional, defaults to true
+  # config.asset_host     = 'http://assets.quintaldetrocas.com.br'
+  config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
