e0ba7c6ddee211a3a8a0c8f68a644e8cbcb4a6dc
Osny Netto <netto@osny.me>
2016-10-18 10:44:17 -0200

    Provide better information for analysis

--- app/admin/exchange.rb
+++ app/admin/exchange.rb
@@ -5,16 +5,31 @@ ActiveAdmin.register Exchange do
 
   index :title => "Trocas" do
     id_column
-    column :toy_from
-    column :toy_to
-    column :user_id
-    column :exchange_type
-    column :exchange_deliver
-    column :finalized
-    column :finalized_at
-    column :accepted
-    column :created_at
-    column :reason
+    column "Solicitante", :user_id do |exc|
+      link_to exc.user.name, edit_admin_user_path(exc.user.id)
+    end
+    column "Destinatário", :toy_to do |exc|
+      link_to exc.to_user.name, edit_admin_user_path(exc.to_toy.id)
+    end
+    column "Brinquedo solicitado", :toy_to do |exc|
+      link_to exc.to_toy.title, toy_path(exc.to_toy)
+    end
+    column "Tipo de troca", :exchange_type do |exc|
+      if exc.exchange_type == "exchange"
+        "Troca"
+      elsif exc.exchange_type == "credit"
+        "Crédito"
+      else
+        "--"
+      end
+    end
+    column "Troca efetuada?", :finalized
+    column "Data troca efetuada", :finalized_at
+    column "Troca aceita?", :accepted
+    column "Troca solicitada em", :created_at
+    column "Troca recusada?", :reason do |exc|
+      exc.reason.blank? ? "--" : raw("Sim<br>Motivo: #{exc.reason}")
+    end
     
     actions
   end
