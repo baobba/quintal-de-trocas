ade113a72ec94624b53244f580b8ada3d9859aad
Osny Netto <netto@osny.me>
2016-10-24 09:17:32 -0200

    Change email message

--- app/mailers/quintal_mailer.rb
+++ app/mailers/quintal_mailer.rb
@@ -49,7 +49,7 @@ class QuintalMailer < ApplicationMailer
   def toy_reminder(toy)
     @toy = toy
     @user = toy.user
-    mail(to: @user.email, subject: 'Seu brinquedo vai expirar em 2 dias, ative-o novamente.')
+    mail(to: @user.email, subject: 'Seu brinquedo está prestes a expirar')
   end
 
   def toy_arrived(exchange, user)
